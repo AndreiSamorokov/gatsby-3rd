31e44976a6609b6436343b5d5b541b18f2137837
AndreiSamorokov <andreiv0202@gmail.com>
2020-10-03 01:53:43 +0900

    update with created layout component

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,13 @@
 import React from "react"
+import Layout from "../components/layout"
+
 export default function About() {
   return (
-    <div>
+    <Layout>
       <h1>About Andrei</h1>
       <p>
         I’m good enough, I’m smart enough, people like me!
       </p>
-    </div>
+    </Layout>
   )
 }
