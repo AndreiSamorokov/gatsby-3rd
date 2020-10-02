b8bbed2847a9022648e58a5952ca57089013a906
AndreiSamorokov <andreiv0202@gmail.com>
2020-10-03 01:32:16 +0900

    created about & contact page

---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+import React from "react"
+export default function About() {
+  return (
+    <div>
+      <h1>About Andrei</h1>
+      <p>
+        I’m good enough, I’m smart enough, people like me!
+      </p>
+    </div>
+  )
+}
